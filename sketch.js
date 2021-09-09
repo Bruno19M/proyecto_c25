e9359568e01eb87a6865e4307ab24ac26edf8e26
Bruno19M <88945532+Bruno19M@users.noreply.github.com>
2021-09-09 16:25:32 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -36,6 +36,6 @@ function draw() {
 
 function keyPressed(){
 	if(keyCode === UP_ARROW) {
-		Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:130,y:-145})
+		Matter.Body.applyForce(paper1.body,paper1.body.position,{x:130,y:-145})
 	}
 }
